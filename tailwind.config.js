8d5da9ac80b64ca93f2ad187ddf9a589d3cc1b4c
p3ngdump <opnkj67@naver.com>
2025-08-12 13:40:45 +0900

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html','./src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        egen: { 500: '#ff499e', 600:'#ff2f8f' },
+        teto: { 500: '#2563eb', 600:'#1d4ed8' }
+      }
+    }
+  },
   plugins: [],
 }
