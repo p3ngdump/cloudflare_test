544cf556af5b52f5155e93fae7576425889d7c23
p3ngdump <opnkj67@naver.com>
2025-08-12 13:12:14 +0900

    Add files via upload

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,4 +1,2 @@
 import * as React from 'react'
-export function Separator() {
-  return <div className="h-px w-full bg-slate-200" />
-}
+export function Separator() { return <div className="h-px w-full bg-slate-200" /> }
